dc1fa0a9e5103a6b4900ae4031d886834c3e13ac
Ashok Koda <kodaashok60@gmail.com>
2022-03-10 20:22:25 +0530

    Added:Adding new contacts

--- src/main/java/com/bridgelabz/controller/AddressBook.java
+++ src/main/java/com/bridgelabz/controller/AddressBook.java
@@ -9,7 +9,7 @@ public class AddressBook {
 		
 		AddressBookService services = new AddressBookService();
 		System.out.println("Create New Contact");
-		//services.createContact(null);
+		services.createContact(null);
 
 	}
 
